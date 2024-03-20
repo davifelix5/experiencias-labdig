e6aecd0791e38ca9bf469946012b6e9e89c687d4
Davi Felix <davifelix@usp.br>
2024-03-19 23:30:34 -0300

    :sparkles: Ajustando frequências do buzzer

--- buzzer.v
+++ buzzer.v
@@ -13,32 +13,32 @@ module buzzer #(parameter CLOCK_FREQ, TOM=4) (
     wire [0:11] pulsos[0:3];
     wire [3:0]  seletor_pulso;
     
-    parameter N = 12, SIZE = 12;
+    parameter N = 12, SIZE = 13;
     // Array de frequências
-    parameter [0:(N*SIZE-1)] frequencias1 = {
-        12'd264, 12'd297, 12'd330, 
-        12'd352, 12'd396, 12'd440, 
-        12'd495, 12'd528, 12'd345, 
-        12'd345, 12'd345, 12'd345
+    parameter [(N*SIZE-1):0] frequencias1 = {
+        13'd523, 13'd554, 13'd587, 
+        13'd622, 13'd659, 13'd698, 
+        13'd734, 13'd783, 13'd830, 
+        13'd880, 13'd932, 13'd988
     };
 
-    parameter [0:(N*SIZE-1)] frequencias2 = {
-        12'd365, 12'd297, 12'd330, 
-        12'd352, 12'd396, 12'd440, 
-        12'd495, 12'd528, 12'd520, 
-        12'd520, 12'd520, 12'd520
+    parameter [(N*SIZE-1):0] frequencias2 = {
+        13'd1046, 13'd1108, 13'd1174, 
+        13'd1244, 13'd1318, 13'd1397, 
+        13'd1480, 13'd1568, 13'd1661, 
+        13'd1760, 13'd1864, 13'd1975
     };
-    parameter [0:(N*SIZE-1)] frequencias3 = {
-        12'd264, 12'd297, 12'd210, 
-        12'd352, 12'd396, 12'd440, 
-        12'd495, 12'd528, 12'd201, 
-        12'd201, 12'd201, 12'd201
+    parameter [(N*SIZE-1):0] frequencias3 = {
+        13'd2093, 13'd2217, 13'd2349, 
+        13'd2489, 13'd2637, 13'd2793, 
+        13'd2960, 13'd3136, 13'd3322, 
+        13'd3502, 13'd3729, 13'd3951
     };
-    parameter [0:(N*SIZE-1)] frequencias4 = {
-        12'd264, 12'd297, 12'd182, 
-        12'd352, 12'd396, 12'd440, 
-        12'd495, 12'd528, 12'd201, 
-        12'd201, 12'd201, 12'd201
+    parameter [(N*SIZE-1):0] frequencias4 = {
+        13'd4186, 13'd4435, 13'd4698, 
+        13'd4978, 13'd5274, 13'd5587, 
+        13'd5919, 13'd6271, 13'd6645, 
+        13'd7040, 13'd7438, 13'd7902
     };
 
     generate
